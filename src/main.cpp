ac7bde1e3046881ee4e1d90c2900fec05ac80e68
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-10-14 19:44:28 -0500

    Refactor

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <unordered_map>
 #include <algorithm>
 #include <iomanip>
+#include <limits>
+#include <vector>
 
 using std::cin;
 using std::cout;
@@ -9,7 +11,10 @@ using std::cerr;
 using std::endl;
 using std::min;
 using std::setw;
-using namespace std;
+using std::vector;
+using std::numeric_limits;
+
+typedef vector<vector<double>> matrix;
 
 struct Edge {
     int startingVertex;
@@ -17,85 +22,121 @@ struct Edge {
     double weight;
 };
 
-int main() {
+struct Input {
     int numberOfVertices;
     int numberOfEdges;
+    vector<Edge> edges;
+};
+
+Input getInput() {
+    Input input = {};
 
     cerr << "How many vertices?" << endl;
-    cin >> numberOfVertices;
+    cin >> input.numberOfVertices;
 
     cerr << "How many edges?" << endl;
-    cin >> numberOfEdges;
+    cin >> input.numberOfEdges;
 
-    double matrix[numberOfVertices][numberOfVertices];
-    memset(matrix, 0, sizeof(matrix));
+    input.edges.resize(static_cast<unsigned long>(input.numberOfEdges));
 
-    Edge edges[numberOfEdges];
-
-    for (int i = 0; i < numberOfEdges; i++) {
+    for (int i = 0; i < input.numberOfEdges; i++) {
         int startingVertex;
         int destinationVertex;
         double weight;
 
-        cout << "Starting vertex for edge " << i + 1 << endl;
+        cerr << "Starting vertex for edge " << i + 1 << endl;
         cin >> startingVertex;
-        cout << "Destination vertex for edge " << i + 1 << endl;
+        cerr << "Destination vertex for edge " << i + 1 << endl;
         cin >> destinationVertex;
-        cout << "Weight for edge " << i + 1 << endl;
+        cerr << "Weight for edge " << i + 1 << endl;
         cin >> weight;
 
-        matrix[startingVertex][destinationVertex] = weight;
-        edges[i] = {
+        input.edges[i] = {
                 startingVertex,
                 destinationVertex,
                 weight
         };
     }
 
-    cout << endl << endl;
+    return input;
+}
 
-    for (int i = 0; i < numberOfVertices; i++) {
-        for (int j = 0; j < numberOfVertices; j++) {
-            cout << setw(2) << matrix[i][j];
-        }
-        cout << endl;
-    }
+matrix initializeMatrix(Input input) {
+    matrix matrix(static_cast<unsigned long>(input.numberOfVertices), vector<double>(static_cast<unsigned long>(input.numberOfVertices), 0));
 
-    //
-    for (int k = 0; k < numberOfVertices - 1; k++) {
-        for (int i = 0; i < numberOfVertices - 1; i++) {
-            for (int j = 0; j < numberOfVertices - 1; j++) {
-                matrix[i][j] = min(matrix[i][j], matrix[i][k] + matrix[k][j]);
+    for (int i = 0; i < input.numberOfVertices; i++) {
+        for (int j = 0; j < input.numberOfVertices; j++) {
+            if (i == j) {
+                matrix[i][j] = 0;
+            } else {
+                matrix[i][j] = std::numeric_limits<double>::infinity();
             }
         }
     }
 
-    cout << endl << endl;
+    for (auto &edge : input.edges) {
+        matrix[edge.startingVertex][edge.destinationVertex] = edge.weight;
+    }
 
-    for (int i = 0; i < numberOfVertices; i++) {
-        for (int j = 0; j < numberOfVertices; j++) {
-            cout << setw(2) << matrix[i][j];
+    return matrix;
+}
+
+void printMatrix(matrix matrix) {
+    for (auto &row : matrix) {
+        for (auto &value : row) {
+            cout << setw(4) << value;
         }
         cout << endl;
     }
+}
 
+void printResults(Input input, matrix matrix) {
     Edge shortestEdge = {
             -1,
             -1,
             -1
     };
-    for (int i = 0; i < numberOfEdges; i++) {
-        int startingVertex = edges[i].startingVertex;
-        int destinationVertex = edges[i].destinationVertex;
+
+    for (int i = 0; i < input.numberOfEdges; i++) {
+        int startingVertex = input.edges[i].startingVertex;
+        int destinationVertex = input.edges[i].destinationVertex;
         double shortestPathWeight = matrix[startingVertex][destinationVertex];
-        cout << "Shortest path from " << startingVertex << " to " << destinationVertex << " costs " << shortestPathWeight
-             << endl;
         if (shortestEdge.weight == -1 || shortestPathWeight < shortestEdge.weight) {
-            shortestEdge = edges[i];
+            shortestEdge = input.edges[i];
         }
+        cout << shortestPathWeight << endl;
     }
 
-    cout << "The shortest path overall is " << shortestEdge.startingVertex << " to " << shortestEdge.destinationVertex
-         << " costing " << shortestEdge.weight << endl;
+    cout << shortestEdge.startingVertex << " " << shortestEdge.destinationVertex << " " << shortestEdge.weight << endl;
+}
 
+void findAllPaths(matrix matrix) {
+    unsigned long size = matrix.size();
+    for (int k = 0; k < size; k++) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                double newValue = matrix[i][k] + matrix[k][j];
+                if (newValue < matrix[i][j]) {
+                    matrix[i][j] = newValue;
+                }
+            }
+        }
+    }
+}
+
+int main() {
+    Input input = getInput();
+    matrix matrix = initializeMatrix(input);
+
+    cout << "Original matrix" << endl;
+    printMatrix(matrix);
+    cout << endl << endl;
+
+    findAllPaths(matrix);
+
+    cout << "Shortest path matrix" << endl;
+    printMatrix(matrix);
+    cout << endl << endl;
+
+    printResults(input, matrix);
 }
