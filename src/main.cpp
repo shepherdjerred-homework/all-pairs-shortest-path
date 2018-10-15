3141a48b4034251da09c23671a1b9fe6fb36e67f
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-10-15 13:48:40 -0500

    Remove unneeded code, readd OMP statement

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,4 @@
 #include <iostream>
-#include <unordered_map>
-#include <algorithm>
-#include <iomanip>
 #include <limits>
 #include <vector>
 #include <omp.h>
@@ -10,16 +7,8 @@ using std::cin;
 using std::cout;
 using std::cerr;
 using std::endl;
-using std::min;
-using std::setw;
 using std::vector;
 using std::numeric_limits;
-using std::chrono::time_point;
-using std::chrono::duration;
-using std::chrono::high_resolution_clock;
-using std::chrono::duration_cast;
-using std::chrono::milliseconds;
-using std::ios_base;
 
 typedef vector<vector<double>> matrix;
 
@@ -41,12 +30,12 @@ Input getInput() {
     cin >> input.numberOfVertices;
     cin >> input.numberOfEdges;
 
-//#pragma omp parallel
+#pragma omp parallel
     {
-//#pragma omp single nowait
+#pragma omp single nowait
         input.edges.resize(static_cast<unsigned long>(input.numberOfEdges));
 
-//#pragma omp single
+#pragma omp single
         for (int i = 0; i < input.numberOfEdges; i++) {
             int startingVertex;
             int destinationVertex;
@@ -71,8 +60,6 @@ matrix initializeMatrix(Input &input) {
     matrix matrix(static_cast<unsigned long>(input.numberOfVertices),
                   vector<double>(static_cast<unsigned long>(input.numberOfVertices), 0));
 
-    // this seemed to make the program slower
-//#pragma omp parallel for collapse(2) if (input.numberOfVertices > 500)
     for (int i = 0; i < input.numberOfVertices; i++) {
         for (int j = 0; j < input.numberOfVertices; j++) {
             if (i == j) {
@@ -83,21 +70,11 @@ matrix initializeMatrix(Input &input) {
         }
     }
 
-    // could parallelize, but would only help when there are a large number of edges
     for (auto &edge : input.edges) {
         matrix[edge.startingVertex][edge.destinationVertex] = edge.weight;
     }
 
     return matrix;
-}
-
-void printMatrix(matrix &matrix) {
-    for (auto &row : matrix) {
-        for (auto &value : row) {
-            cerr << setw(4) << value;
-        }
-        cerr << endl;
-    }
 }
 
 void printResults(Input &input, matrix &matrix) {
@@ -123,8 +100,7 @@ void printResults(Input &input, matrix &matrix) {
 void findAllPaths(matrix &matrix) {
     unsigned long size = matrix.size();
 
-    // use static scheduling since each iteration should have roughly the same amount of work
-//#pragma omp parallel for schedule(static) collapse(3)
+#pragma omp parallel for schedule(static) collapse(3)
     for (int k = 0; k < size; k++) {
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
@@ -144,14 +120,6 @@ void run() {
     printResults(input, matrix);
 }
 
-// these didn't seem to
-// http://codeforces.com/blog/entry/5217
-void ioHacks() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-}
-
 int main() {
-//    ioHacks();
     run();
 }
